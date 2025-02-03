5a194796ee99c5625d113268f365895d69df8b9d
Kevyn FLORESDACOSTA <kev.floresdacosta@ecoles-epsi.net>
2025-02-03 14:28:11 +0100

    Ajout de variables CSS pour le style du footer et création de classes pour le fond et le texte du footer. Ajout de nouvelles icônes et images vectorielles.

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,19 +1,19 @@
+import React from 'react';
+
 export default function Footer() {
   return (
-    <div className="bg-[#0077b6] flex flex-col items-center justify-center h-[94px] relative">
-      <div className="text-white text-center font-['Itim-Regular',_sans-serif] text-2xl font-normal">
+    <div className="bg-footer text-footer py-4 flex justify-between items-center">
+      <div className="flex space-x-4 ml-4">
+        <img className="w-8 h-8" src={`${process.env.PUBLIC_URL}/Assets/Logo/Instagram.png`} alt="Instagram" />
+        <img className="w-8 h-8" src={`${process.env.PUBLIC_URL}/Assets/Logo/Facebook.png`} alt="Facebook" />
+        <img className="w-8 h-8" src={`${process.env.PUBLIC_URL}/Assets/Logo/Discord.png`} alt="Discord" />
+      </div>
+      <div className="text-center">
         © 2024 ShieldSearch. All rights reserved.
       </div>
-      <div className="flex justify-between w-full px-8 mt-4">
-        <div className="flex space-x-8">
-          <img className="w-[50px] h-[50px]" src="instagram-1-10.png" alt="Instagram" />
-          <img className="w-[50px] h-[50px]" src="facebook-10.png" alt="Facebook" />
-          <img className="w-[50px] h-[50px]" src="discord-10.png" alt="Discord" />
-        </div>
-        <div className="flex space-x-8">
-          <img className="w-[50px] h-[50px]" src="apple-10.png" alt="Apple Store" />
-          <img className="w-[50px] h-[50px]" src="android-10.png" alt="Google Play" />
-        </div>
+      <div className="flex space-x-4 mr-4">
+        <img className="w-8 h-8" src={`${process.env.PUBLIC_URL}/Assets/Logo/Android.png`} alt="Google Play" />
+        <img className="w-8 h-8" src={`${process.env.PUBLIC_URL}/Assets/Logo/Apple.png`} alt="Apple Store" />
       </div>
     </div>
   );
--- src/pages/Home/Header/Header.js
+++ src/pages/Home/Header/Header.js
@@ -1,47 +1,9 @@
+import React from 'react';
+
 export default function Header() {
   return (
-    <div className="bg-[#0077b6] flex flex-col items-center justify-center h-[902px]">
-      <img
-        className="w-full h-[61.39px] overflow-visible"
-        src="vector-10.svg"
-        alt="Décoration vectorielle"
-      />
-      <div className="flex flex-col items-center">
-        <div className="relative">
-          <img
-            className="absolute left-[980px] top-[444.95px] transform rotate-[-64.598deg]"
-            src="group0.svg"
-            alt="Icône décoration"
-          />
-          <img
-            className="absolute left-[388px] top-[328px]"
-            src="highlight-050.svg"
-            alt="Icône mise en avant"
-          />
-          <div className="text-white text-center font-['Itim-Regular',_sans-serif] text-5xl font-normal">
-            Nous sommes ShieldSearch
-          </div>
-        </div>
-        <img
-          className="w-[500px] h-[500px] object-cover"
-          src="azur-3-removebg-preview-10.png"
-          alt="Illustration principale"
-        />
-        <div className="text-white text-center font-['Itim-Regular',_sans-serif] text-xl font-normal mt-4">
-          ShieldSearch c’est deux étudiants en école d’ingénieur en informatique.
-          Dans le cadre de nos études, nous avons voulu créer un moyen de se protéger face aux cybermenaces actuelles.
-          <br />
-          Nous avons pensé au QR code, de plus en plus utilisé au quotidien, qui nous renvoie généralement directement sur une page après le scan.
-          <br />
-          C’est pourquoi nous avons décidé de créer une plateforme permettant de générer des mots de passe robustes et de vérifier nos anciens mots de passe.
-          ShieldSearch permet également de vérifier des liens sur la version web et d’analyser les liens derrière les QR codes.
-        </div>
-      </div>
-      <img
-        className="w-[170.07px] h-[83.44px] mt-4"
-        src="vector-217-stroke0.svg"
-        alt="Décoration vectorielle"
-      />
+    <div>
+      
     </div>
   );
 }
